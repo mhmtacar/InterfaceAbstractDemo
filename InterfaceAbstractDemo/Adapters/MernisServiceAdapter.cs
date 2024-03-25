18e402df244f569c2681af733bb348bb391978e0
Mehmet <mehmet-acar-gs@hotmail.com>
2024-03-25 03:22:51 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync
+                (new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(Convert.ToInt64(customer.NationalityId), customer.FirstName, customer.LastName, customer.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+customerManager.Save(new Customer { DateOfBirthYear = 1999, FirstName = "Mehmet", LastName = "Acar", NationalityId = "12345678910" });
